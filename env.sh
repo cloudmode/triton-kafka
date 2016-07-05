33d9d4b29d5ec429f01bac5868045002db77db8c
mike <mike@cloudmo.de>
2016-07-04 17:19:33 -0700

    First Commit

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get network info from consul and poll it for liveness
+if [ -z "$1" ]; then
+    CONSUL_IP=$(sdc-listmachines --name consul_consul_1 | json -a ips.1)
+else
+    CONSUL_IP=${CONSUL_IP:-$(docker-machine ip default)}
+fi
+echo CONSUL=$CONSUL_IP > _env
